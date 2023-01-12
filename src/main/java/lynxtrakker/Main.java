d6f950ae0b08fba8788223ad8005f26776984396
lynxtrakker <lynxtrakker@protonmail.com>
2023-01-11 23:25:40 -0600

    Work added to the eventListener and Main file

--- src/main/java/lynxtrakker/Main.java
+++ src/main/java/lynxtrakker/Main.java
@@ -7,24 +7,38 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
+import javax.security.auth.login.LoginException;
+
 public class Main extends ListenerAdapter {
-    public static long start = System.currentTimeMillis();
+    public static long start = System.currentTimeMillis(); // Creates the start time that we are going to be used to test how long the bot takes to start up
+    private final ShardManager shardManager; // Creating a private variable called shardManager and also making it final so it cannot be changed
+    public Main() throws LoginException {
+        //Token for the bot is placed into a string for later use
+        String TOKEN = "ODMxNTM5NjQzNDc1Njg5NTAy.GBekrF.Z5rlioyubSwEQWFu8lqugQEdrTWM8VYiR_wMk4";
+        //This builds the shard manager and gives the manager the token to link with Discord
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(TOKEN);
+        //Sets options for the builder
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setActivity(Activity.streaming("Moistcr1tikal", "https://www.twitch.tv/moistcr1tikal"));
+        shardManager = builder.build(); // We are building the builder into a variable called shardManager
+        shardManager.addEventListener(new eventListener()); //Adds the main event listening file to the bot
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
     public static void main(String[] args) {
         try {
-            JDABuilder builder = JDABuilder.createDefault("ODMxNTM5NjQzNDc1Njg5NTAy.GBekrF.Z5rlioyubSwEQWFu8lqugQEdrTWM8VYiR_wMk4")
-                    .setAutoReconnect(true)
-                    .setActivity(Activity.playing("FORTNITE BATTLE ROYALE ON MY SAMSUNG SMART FRIDGE"))
-                    .setStatus(OnlineStatus.DO_NOT_DISTURB)
-                    .disableCache(CacheFlag.ACTIVITY)
-                    .setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER))
-                    .setChunkingFilter(ChunkingFilter.NONE)
-                    .enableIntents(GatewayIntent.GUILD_MEMBERS);
-            JDA jda = builder.build();
-            jda.addEventListener(new eventListener());
+            Main bot = new Main();
+
+
         } catch (Exception e){
             e.printStackTrace();
         }
--- src/main/java/lynxtrakker/events/eventListener.java
+++ src/main/java/lynxtrakker/events/eventListener.java
@@ -1,11 +1,30 @@
 package lynxtrakker.events;
 
+import lynxtrakker.Main;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
 public class eventListener implements EventListener {
     @Override
     public void onEvent(GenericEvent event) {
+        //
+        JDA jda = event.getJDA();
+        if (event instanceof ReadyEvent){
+            long end = System.currentTimeMillis() - Main.start;
+            System.out.printf("[%s] Now up and running. Took %s ms to load and ping is %s ms\n%s\n", event.getJDA().getSelfUser().getName(), end, event.getJDA().getGatewayPing(), event.getJDA().getGuilds());
+            TextChannel textChannel = jda.getTextChannelById(753248587420401757L);
 
+        }
+        if (event instanceof MessageReceivedEvent){
+            if (!((MessageReceivedEvent) event).getAuthor().isBot()) {
+                Message message = ((MessageReceivedEvent) event).getMessage();
+                String messageContent = ((MessageReceivedEvent) event).getMessage().getContentDisplay().toLowerCase();
+            }
+        }
     }
 }
